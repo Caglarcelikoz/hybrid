9f3566ec3cb4cb5a04d517725b30654ef01a6bd1
caglarcelikoz <caglarcelikoz@outlook.be>
2020-05-07 23:25:01 +0200

    upgraded to 1.6.3

--- public/app.ts
+++ public/app.ts
@@ -9,7 +9,13 @@ app.run(function($rootScope, $location) {
     }
   });
 });
-
 angular.element(document).ready(() => {
   angular.bootstrap(document.body, ["app"]);
 });
+
+app.config([
+  "$locationProvider",
+  function($locationProvider) {
+    $locationProvider.hashPrefix("");
+  }
+]);
